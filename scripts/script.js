d80ed40829a97ed3777ea32a69455c421ce1e402
Alexandr Zavitaev <inmcommon@gmail.com>
2020-12-01 19:12:32 +0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ let addButton = document.querySelector('.profile__edit-button');
 let resetButton = document.querySelector('.popup__submit-button');
 let formName = document.querySelector('.profile__title');
 let formAbout = document.querySelector('.profile__subtitle');
-let popupName = document.querySelector('.popup__input-area_name');
-let popupAbout = document.querySelector('.popup__input-area_about');
+let popupName = document.querySelector('.popup__name');
+let popupAbout = document.querySelector('.popup__about');
 let formElement = document.querySelector('.popup__elements');
 
 function openPopup() {
